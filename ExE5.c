1ddca12415feb16a6ce0d293a1392a7c1d220dee
Adam Khobba <akhobba@e1r13p6.1337.ma>
2023-11-13 21:50:48 +0100

    hI

---
+++ ExE5.c
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+
+int main ()
+{
+// 1)
+
+	size_t	i;
+	size_t	n;
+
+	printf("taper le number de dimension de tableau");
+	scanf("%d", n);
+	printf("remplir le tableau mais si tu veut passe a une dimension a une autre taper espace ' ' ^ @_@");
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (1)
+		{
+			printf("Dimeension %d", i);
+			scanf ("%d",T[i][j]);
+			if ( )
+				break ;
+		}
+		i++;
+	}
+}
